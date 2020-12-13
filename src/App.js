b0b53692ceaebc6f8ef3b457919d4af519b5b71f
Paul Billings <paulbillings101@yahoo.co.uk>
2020-12-13 14:31:49 +0000

    added api to get info for celebrity birthdays

--- src/App.js
+++ src/App.js
@@ -1,20 +1,68 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import data from "./data";
 import List from "./List";
+// import apiData from "./apiData";
+let apiPeopleData = {};
+
+const url = "https://celebrity-bucks.p.rapidapi.com/birthdays/JSON";
+
 function App() {
-  const [people, setPeople] = useState(data);
+  const [celebrities, setCelebs] = useState([]);
+
+  const API_KEY = process.env.REACT_APP_RAPIDAPI_KEY;
+
+  const celebrityPeople = async () => {
+    const response = await fetch(url, {
+      method: "GET",
+      headers: {
+        "x-rapidapi-key": API_KEY,
+        "x-rapidapi-host": "celebrity-bucks.p.rapidapi.com",
+      },
+    });
+    const celebrities = await response.json();
+    setCelebs(celebrities.Birthdays);
+  };
+
+  useEffect(() => {
+    celebrityPeople();
+  }, []);
+
   return (
     <main>
       <section className="container">
-        <h3>{people.length} Birthdays today</h3>
-        <List people={people} />
-        <button
-          onClick={() => {
-            setPeople([]);
-          }}
-        >
-          Clear All
-        </button>
+        <h2>Celebrity Birthdays</h2>
+        <p>Found {celebrities.length} Today</p>
+        <ul className="users">
+          {celebrities.map((celeb) => {
+            console.log(celeb);
+            const { celebId, name, age } = celeb;
+            const img = `https://celebritybucks.com/images/celebs/mid/${celebId}.jpg`;
+            return (
+              <li key={celebId}>
+                <img
+                  src={img}
+                  alt="Avatar"
+                  // if no image for celeb
+                  onError={(e) => {
+                    e.target.onerror = null;
+                    e.target.src =
+                      "https://celebritybucks.com/images/celebs/mid/default.gif";
+                  }}
+                />
+                <div>
+                  <h3>{name}</h3>
+                  <p>Age: {age}</p>
+                  <a
+                    href={`https://en.wikipedia.org/wiki/${name}`}
+                    target="_blank"
+                  >
+                    <button className="btn">More Info</button>
+                  </a>
+                </div>
+              </li>
+            );
+          })}
+        </ul>
       </section>
     </main>
   );
---
+++ src/apiData.js
@@ -0,0 +1,21 @@
+let apiPeople = {};
+
+// fetch("https://celebrity-bucks.p.rapidapi.com/birthdays/JSON", {
+//   method: "GET",
+//   headers: {
+//     "x-rapidapi-key": "6b9154ed18msh245ed81ac5ac278p1ef1d7jsnf6fe2f3df1de",
+//     "x-rapidapi-host": "celebrity-bucks.p.rapidapi.com",
+//   },
+// })
+//   .then((response) => {
+//     response.json().then((data) => {
+//       // console.log(data);
+//       apiPeople = data;
+//       console.log(data.Birthdays[0]);
+//     });
+//   })
+//   .catch((err) => {
+//     console.error(err);
+//   });
+
+export default apiPeople;
